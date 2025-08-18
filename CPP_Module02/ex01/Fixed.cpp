9c7dd73e9ea76fcb2de2bbe578f00a6ae5c54c96
KuroeProg <itsumy81@gmail.com>
2025-08-18 13:24:30 +0200

    module02 - ex01 - WIP

---
+++ CPP_Module02/ex01/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+	this->_nbr = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copie) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->_nbr = copie.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &copie) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copie)
+		this->_nbr = copie.getRawBits();
+	return *this;
+}
+
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return _nbr;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->_nbr = raw;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
+	out << fixed.toFloat();
+	return out;
+}
---
+++ CPP_Module02/ex01/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 11:32:59 by cfiachet          #+#    #+#             */
+/*   Updated: 2025/08/18 13:14:06 by cfiachet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int _nbr;
+		static const int fractionnalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const int n);
+		Fixed (const float f);
+		Fixed(const Fixed &copie);
+		Fixed &operator=(const Fixed &copie);
+		~Fixed();
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int 	toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed); //thx gpt
+
+#endif
