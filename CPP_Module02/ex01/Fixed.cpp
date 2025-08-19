3de34d102293ed1765607e09af9a377545815ed3
KuroeProg <itsumy81@gmail.com>
2025-08-19 14:21:51 +0200

    cpp_mod02-ex01

--- CPP_Module02/ex01/Fixed.cpp
+++ CPP_Module02/ex01/Fixed.cpp
@@ -7,21 +7,23 @@ Fixed::Fixed() {
 
 Fixed::Fixed(const Fixed &copie) {
 	std::cout << "Copy constructor called" << std::endl;
-	this->_nbr = copie.getRawBits();
+	*this = copie;
 }
 
 Fixed::Fixed(int const n) {
+	std::cout << "Int constructor called" << std::endl;
 	this->_nbr = n << fractionnalBits;
 }
 
 Fixed::Fixed(float const f) {
+	std::cout << "Float constructor called" << std::endl;
 	this->_nbr = roundf(f * (1 << fractionnalBits));
 }
 
 Fixed &Fixed::operator=(const Fixed &copie) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &copie)
-		this->_nbr = copie.getRawBits();
+		this->_nbr = copie._nbr;
 	return *this;
 }
 
@@ -30,10 +32,9 @@ Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
 
-int		Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
-	return _nbr;
-}
+// int		Fixed::getRawBits(void) const {
+// 	return _nbr;
+// }
 
 void	Fixed::setRawBits(int const raw) {
 	this->_nbr = raw;
