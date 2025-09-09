47916aa8cf22182294f264add23062cc4ad63e80
KuroeProg <itsumy81@gmail.com>
2025-09-09 08:12:18 +0200

    upgraded phonebook

--- CPP_Module00/ex01/add.cpp
+++ CPP_Module00/ex01/add.cpp
@@ -8,7 +8,11 @@ int		check_number(const std::string &str) {
 		std::cout << "The number is too long" << std::endl;
 		return 0;
 	}
-	for (size_t i = 0; i < str.size(); i++) {
+	if (!(std::isdigit(str[0])) || str[0] != '+')
+		return 1;
+	for (size_t i = 1; i < str.size(); i++) {
+		if (!str[0])
+			return 0;
 		if (!(std::isdigit(str[i]))) {
 			std::cout << "invalid number" << std::endl;
 			return 0;
@@ -24,20 +28,36 @@ int	Contact::fill_contact()
 	std::cout << "Enter first name" << std::endl;
 	std::getline(std::cin, input);
 	// std::cin >> input;
+	if (input == "") {
+		std::cout << "Input can't be empty" << std::endl;
+		return 1;
+	}
 	this->firstName = input;
 
 	std::cout << "Enter last name" << std::endl;
 	std::getline(std::cin, input);
+	if (input == "") {
+		std::cout << "Input can't be empty" << std::endl;
+		return 1;
+	}
 	// std::cin >> input;
 	this->lastName = input;
 
 	std::cout << "Enter nickname" << std::endl;
 	std::getline(std::cin, input);
+	if (input == "") {
+		std::cout << "Input can't be empty" << std::endl;
+		return 1;
+	}
 	// std::cin >> input;
 	this->nickname = input;
 
 	std::cout << "Enter number" << std::endl;
 	std::getline(std::cin, input);
+	if (input == "") {
+		std::cout << "Input can't be empty" << std::endl;
+		return 1;
+	}
 	// std::cin >> input;
 	if (!check_number(input)) 
 		return 1;
@@ -45,6 +65,10 @@ int	Contact::fill_contact()
 	
 	std::cout << "Enter darkest secret" << std::endl;
 	std::getline(std::cin, input);
+	if (input == "") {
+		std::cout << "Input can't be empty" << std::endl;
+		return 1;
+	}
 	// std::cin >> input;
 	this->darkestSecret = input;
 
