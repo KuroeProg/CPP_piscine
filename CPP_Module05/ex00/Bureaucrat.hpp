07cd4f81428a539d88e80470a3f959d3212d846f
KuroeProg <itsumy81@gmail.com>
2025-09-25 00:53:52 +0200

    Module05 - ex00

---
+++ CPP_Module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 09:46:40 by cfiachet          #+#    #+#             */
+/*   Updated: 2025/09/11 19:10:25 by cfiachet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <stdexcept>
+
+class Bureaucrat {
+	private:
+		const std::string	_name;
+		int					_grade;
+
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public :
+				const char *what() const throw();
+		};
+
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(const Bureaucrat &copie);
+	Bureaucrat	&operator=(const Bureaucrat &other);
+	~Bureaucrat();
+
+	const std::string	&getName() const;
+	int					getGrade() const;
+	void				incrementGrade();
+	void				decrementGrade();
+
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &b);
+
+#endif
---
+++ CPP_Module05/ex00/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/24 21:51:37 by cfiachet          #+#    #+#             */
+/*   Updated: 2025/09/25 00:53:13 by cfiachet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main() {
+	Bureaucrat timmy("Timmy", 42);
+	Bureaucrat angel("angel", 150);
+	Bureaucrat error2("Error2", 1);
+	
+	std::cout << timmy << std::endl;
+	std::cout << angel << std::endl;
+	std::cout << error2 << std::endl;
+
+	try {
+		timmy.incrementGrade();
+	} catch (std::exception &e) {
+		std::cerr << "Exception caught : "  << e.what() << std::endl;
+	}
+	try {
+		angel.decrementGrade();
+	} catch (std::exception &e) {
+		std::cerr << "Exception caught : " << e.what() << std::endl;
+	}
+	try {
+		error2.incrementGrade();
+	} catch (std::exception &e) {
+		std::cerr << "Exception caught : " << e.what() << std::endl;
+	}
+
+	std::cout << "After the changement of grades" << std::endl;
+	std::cout << timmy << std::endl;
+	std::cout << angel << std::endl;
+	std::cout << error2 << std::endl;
+	
+	return 0;
+}
